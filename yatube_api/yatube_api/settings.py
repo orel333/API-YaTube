484410620ffc05e61e42bb385aebbec41ec8f9d2
Alexey Orel <58585466+orel333@users.noreply.github.com>
2022-04-13 23:18:00 +0300

    Update settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = 'some_secret_key'
 
 DEBUG = True
 
